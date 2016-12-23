744b36814c066e6d3dc4d91ec7a38a65525d45dd
Lars Bilke <lars.bilke@ufz.de>
2016-12-23 14:34:40 +0100

    Correctly filter out debug mac builds.

--- build.py
+++ build.py
@@ -5,12 +5,10 @@ import platform
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="bilke", channel="testing")
     builder.add_common_builds()
-    builder.add({"arch": "x86_64", "build_type": "Release"}, {"Qt:xmlpatterns": "True"})
+    builder.add({}, {"Qt:xmlpatterns": "True"})
     filtered_builds = []
     for settings, options in builder.builds:
         if os_info.is_macos and settings["build_type"] != "Debug":
             filtered_builds.append([settings, options])
-        if settings["arch"] == "x86_64":
-             filtered_builds.append([settings, options])
     builder.builds = filtered_builds
     builder.run()
