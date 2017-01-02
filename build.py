f169e665000d64a9350bccb39bb802a801e91a23
Lars Bilke <lars.bilke@ufz.de>
2017-01-02 09:00:12 +0100

    Fixed build.py builds.

--- build.py
+++ build.py
@@ -5,10 +5,19 @@ import platform
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="bilke", channel="testing")
     builder.add_common_builds()
-    builder.add({"arch": "x86_64", "build_type": "Release"}, {"Qt:xmlpatterns": "True"})
+
+    # Add xmlpatterns builds
+    builder.add({"arch": "x86_64"}, {"Qt:xmlpatterns": "True"})
+
+    # Filter out
+    #  - macOS Debug builds
+    #  - x32 builds
     filtered_builds = []
     for settings, options in builder.builds:
-        if os_info.is_macos and settings["build_type"] != "Debug":
-            filtered_builds.append([settings, options])
+        if os_info.is_macos and settings["build_type"] == "Debug" \
+            or settings["arch"] == "x86":
+            continue
+        filtered_builds.append([settings, options])
     builder.builds = filtered_builds
+
     builder.run()
