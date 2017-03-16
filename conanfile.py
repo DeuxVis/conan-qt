3433bafad9e55951bf5591e38da025b9e28d294b
Lars Bilke <lars.bilke@ufz.de>
2017-03-16 15:13:23 +0100

    fix

--- conanfile.py
+++ conanfile.py
@@ -35,7 +35,7 @@ class QtConan(ConanFile):
                           "libx11-xcb1", "libx11-xcb-dev", "libxcb-keysyms1",
                           "libxcb-keysyms1-dev", "libxcb-image0", "libxcb-image0-dev",
                           "libxcb-shm0", "libxcb-shm0-dev", "libxcb-icccm4",
-                          "libxcb-icccm4-dev", "${xcb_pack_name}"
+                          "libxcb-icccm4-dev", "${xcb_pack_name}",
                           "libxcb-xfixes0-dev", "libxrender-dev", "libxcb-shape0-dev",
                           "libxcb-randr0-dev", "libxcb-render-util0", "libxcb-render-util0-dev",
                           "libxcb-glx0-dev", "libxcb-xinerama0", "libxcb-xinerama0-dev"]
